3b22a396a1d7009664492fbdfd5b0ffb40c3e434
Rahul <iamrahulrajendran01@gmail.com>
2024-03-03 11:43:02 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Landingpage from './pages/Landingpage';
+
+function App() {
+  return (
+   <div>
+    <Landingpage/>
+   </div>
+  );
+}
+
+export default App;
