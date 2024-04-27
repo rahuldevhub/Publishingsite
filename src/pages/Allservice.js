b8c190b89995abeaaad539b9fc7dec153facfc12
Rahul <iamrahulrajendran01@gmail.com>
2024-04-27 19:44:41 +0530

    Add files via upload

---
+++ src/pages/Allservice.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+
+const Allservice = () => {
+  return (
+    <div className='Allservice'>
+        <p className='title' >Service we offer</p>
+            <p className='detailedabout-subtitle' >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+
+<Row className='characters'>
+                <Col lg='4'>picture</Col>
+                <Col lg='8'>
+                <p className='detailedabout-subtitle' >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+                </Col>
+            </Row>
+
+
+            <Row className='characters'>
+                <Col lg='8'>
+                <p className='detailedabout-subtitle' >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+
+                </Col>
+                <Col lg='4'>
+             picture
+                </Col>
+            </Row>
+
+
+            <Row className='characters'>
+                <Col lg='4'>picture</Col>
+                <Col lg='8'>
+                <p className='detailedabout-subtitle' >It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+            Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+
+                </Col>
+            </Row>
+    </div>
+  )
+}
+
+export default Allservice
---
+++ src/sections/Contactus.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap';
+import Contactusform from '../components/contactusform/Contactusform'
+import '../css/contactus.css'
+import left from '../assets/contactleft.jpg'
+
+import Lottie from 'lottie-react'
+import book from '../assets/Animation/book.json'
+
+const Contactus = () => {
+    return (
+        <div className='Contactus'>
+
+
+
+            <Row >
+                <Col lg="6" md="6" sm="4" className='contact-leftcontent-col'>
+                    <div className='contact-left-animation'>
+                    <Lottie animationData={book} />
+                       
+                    </div>
+                </Col>
+
+
+                <Col lg="6" md="6" sm="4" className='contact-rightcontent-col'>
+                    <p className='title' >Contact us</p>
+                    {/* <p className='subtitle' >We are ready to work on a project of any complexity, whether it’s commercial or residential.</p> */}
+
+                    <Contactusform />
+
+
+                </Col>
+            </Row>
+
+
+
+
+
+
+
+
+
+        </div>
+    )
+}
+
+export default Contactus
