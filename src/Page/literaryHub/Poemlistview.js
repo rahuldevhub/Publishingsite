9dfcbfc21b5fe773e61d63757057e5338ba37ecf
Rahul <iamrahulrajendran01@gmail.com>
2024-12-07 22:25:55 +0530

    Update Poemlistview.js

--- src/Page/literaryHub/Poemlistview.js
+++ src/Page/literaryHub/Poemlistview.js
@@ -14,27 +14,36 @@ export const Poemlistview = () => {
     }, []);
 
     const newpoemdata = [
-        {
+         {
             id: 1,
+            title: 'Sun in Africa',
+            authorname: "Zuri",
+            img: img04,
+            description: "Sun in Africa” paints a vivid picture of Africa, a continent marked by both struggle and resilience. It highlights the harsh realities of poverty and hardship, yet celebrates the unbroken spirit and grace of its people.",
+            posdate: "Dec 5th 2024",
+        },
+        {
+            id: 2,
             title: 'To someone who’s struggling in silence',
             authorname: "Shahitha Fareen M",
             img: img04,
             description: "Author takes readers on an emotional journey through the highs and lows of life. This heartfelt piece emphasizes the importance of surrounding oneself with people who love unconditionally and bring out the best in us. It acknowledges the inevitable encounters with those who may cause pain and doubt but reassures that these negative influences are temporary. ",
             posdate: "Dec 1st 2024",
         },
-        {
-            id: 2,
+      
+    ]
+
+    const poemdata = [
+         {
+            id: 3,
             title: 'Invisible strokes',
             authorname: "Stan Miller",
             img: img03,
             description: "In a grand, empty hall, an artist embarks on creating a masterpiece. Withnoaudience to witness his work, he finds solace and inspiration in solitude. Each brushstroke and color choice becomes a dance and a song, bringing the canvas to life. The poem explores the beauty of creating art for oneself, without the need for external validaion.",
             posdate: "Nov 25th 2024",
         },
-    ]
-
-    const poemdata = [
         {
-            id: 2,
+            id: 4,
             title: "Daddy's little girl",
             authorname: "Zain Al Fikri",
             img: img02,
@@ -42,7 +51,7 @@ export const Poemlistview = () => {
             posdate: "Nov 12th 2024",
         },
         {
-            id: 3,
+            id: 5,
             title: 'Happily Ever After',
             authorname: "Saran Raj",
             img: img01,
